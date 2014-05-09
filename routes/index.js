23ba49df4d617a6e63f603c01b30de9a62cece5a
Ryan McAdler <ryanmcadler@gmail.com>
2014-05-08 20:20:00 -0700

    got simple http request working

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
-
-var options = {
-  host: 'http://google.com',
-  path: '/',
-  port: '80',
-};
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-
-  console.log(options)
-
-  //callback = function(response) {
-  //  console.log(response);
-  //}
-
-  var req = http.request(options);
-  req.end();
+  request(
+    'http://track.mtbachelor.com/tyt.asp?passmediacode=MBA6438294&season=12-13&currentday=null',
+    function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    }
+  });
   res.render('index', { title: 'TurnGrafz' });
 });
 
