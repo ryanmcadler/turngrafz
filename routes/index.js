4fe8a96c2e2cd57127cb2a272dedafc5ccf29931
Ryan McAdler <ryanmcadler@gmail.com>
2014-05-08 21:26:42 -0700

    filtering out grabage table data; pushing relevant data to array

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,24 @@ router.get('/', function(req, res) {
     'http://track.mtbachelor.com/tyt.asp?passmediacode=MBA6438294&season=12-13&currentday=null',
     function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      //var parsedHTML = $.load(body);
-      $ = cheerio.load(body);
-      $('tr').map(function(i, el) {
-        $(this).children('td').map(function(i, el) {
-          if (i == 2) {
-            console.log($(this).text());
-          }
-        })
+      $ = cheerio.load(body); // parse html...
+      var tableRowCount = $('.customerContent').next().children('tr').length;
+      // loop through each row and push values to array...
+      $('.customerContent').next().children('tr').map(function(i, el) {
+        if (i != 0 && i < (tableRowCount-4)) {
+          var row = [];
+          $(this).children('td').map(function(i, el) {
+            if (i == 0) {
+              row.push($(this).text());
+            }
+            if (i == 2) {
+              row.push( parseInt($(this).text()) );
+            }
+          });
+          vert.push(row);
+        }
       });
+      console.log(vert);
     }
   });
   res.render('index', { title: 'TurnGrafz' });
