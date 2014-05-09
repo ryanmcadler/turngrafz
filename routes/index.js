3ecdc01373ae97eeb84f639671e5b012425c8d2f
Ryan McAdler <ryanmcadler@gmail.com>
2014-05-08 20:59:57 -0700

    parsing html like a boss

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
+var cheerio = require('cheerio');
 /* GET home page. */
 router.get('/', function(req, res) {
+  var vert = [];
   request(
     'http://track.mtbachelor.com/tyt.asp?passmediacode=MBA6438294&season=12-13&currentday=null',
     function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body);
+      //var parsedHTML = $.load(body);
+      $ = cheerio.load(body);
+      $('tr').map(function(i, el) {
+        $(this).children('td').map(function(i, el) {
+          if (i == 2) {
+            console.log($(this).text());
+          }
+        })
+      });
     }
   });
   res.render('index', { title: 'TurnGrafz' });
