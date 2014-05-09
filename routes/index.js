590030ab07248b04c389a04ca801bfee775f373e
Ryan McAdler <ryanmcadler@gmail.com>
2014-05-08 20:09:34 -0700

    trying to get http to work

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+
+var options = {
+  host: 'http://google.com',
+  path: '/',
+  port: '80',
+};
 
 /* GET home page. */
 router.get('/', function(req, res) {
+
+  console.log(options)
+
+  //callback = function(response) {
+  //  console.log(response);
+  //}
+
+  var req = http.request(options);
+  req.end();
   res.render('index', { title: 'TurnGrafz' });
 });
 
