9649b198ce4fa94b32bb1a7b17561f9f01798e95
Ryan McAdler <ryanmcadler@gmail.com>
2014-05-13 22:15:03 -0700

    extract out turn data acquisition into seperate function with callbacks

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,12 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index');
-});
-
-router.post('/turngraf', function(req, res) {
-  var passno = req.body.user.passno;
+var getTurnData = function(passno, season, callback) {
   var reqUrl = 'http://track.mtbachelor.com/tyt.asp?passmediacode=' + passno + '&season=13-14&currentday=null'
+  var turns = [];
   var days = [];
   var vert = [];
-  request(reqUrl, function (error, response, body) {
+  request(reqUrl, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       $ = cheerio.load(body); // parse html...
       var tableRowCount = $('.customerContent').next().children('tr').length;
@@ -31,8 +26,22 @@ router.post('/turngraf', function(req, res) {
         }
       });
     }
-    res.render('turngraf', { vert: JSON.stringify(vert), days: JSON.stringify(days) });
+    turns.push(days);
+    turns.push(vert);
+    callback(turns);
   });
+};
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.post('/turngraf', function(req, res) {
+  var passno = req.body.user.passno;
+  getTurnData(passno, '13-14', function(turns){
+    res.render('turngraf', { vert: JSON.stringify(turns[1]), days: JSON.stringify(turns[0]) });
+  })
 });
 
 module.exports = router;
