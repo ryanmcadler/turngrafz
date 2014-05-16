2c8104198dffa6c1fe7d06bfbf609a9aef0daa57
Ryan McAdler <ryanmcadler@gmail.com>
2014-05-15 20:01:46 -0700

    got spline graphs working

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ var cheerio = require('cheerio');
 var getTurnData = function(passno, season, callback) {
   var reqUrl = 'http://track.mtbachelor.com/tyt.asp?passmediacode=' + passno + '&season=13-14&currentday=null'
   var turns = [];
-  var days = [];
-  var vert = [];
   request(reqUrl, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       $ = cheerio.load(body); // parse html...
@@ -15,19 +13,25 @@ var getTurnData = function(passno, season, callback) {
       // loop through each row and push values to array...
       $('.customerContent').next().children('tr').map(function(i, el) {
         if (i != 0 && i < (tableRowCount-4)) {
+          var row = [];
           $(this).children('td').map(function(i, el) {
             if (i == 0) {
-              days.push($(this).text());
+              var dateStr = $(this).text();
+              var firstTick = dateStr.indexOf('/');
+              var secondTick = dateStr.indexOf('/', (firstTick + 1 ));
+              var y = dateStr.slice(secondTick + 1);
+              var m = dateStr.slice(0, firstTick);
+              var d = dateStr.slice((firstTick + 1), secondTick);
+              row.push( Date.UTC(y, m, d) );
             }
             if (i == 2) {
-              vert.push( parseInt($(this).text()) );
+              row.push( parseInt($(this).text()) );
             }
           });
+          turns.push(row);
         }
       });
     }
-    turns.push(days);
-    turns.push(vert);
     callback(turns);
   });
 };
@@ -40,7 +44,7 @@ router.get('/', function(req, res) {
 router.post('/turngraf', function(req, res) {
   var passno = req.body.user.passno;
   getTurnData(passno, '13-14', function(turns){
-    res.render('turngraf', { vert: JSON.stringify(turns[1]), days: JSON.stringify(turns[0]) });
+    res.render('turngraf', { turns: JSON.stringify(turns) });
   })
 });
 
